ea4bf24923bc0c5adba30ea4009b5106c873caaa
MliKiowa <nineto0@163.com>
2022-08-13 17:11:59 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 module.exports = OnRequest;
 var fs = require("fs")
-
-function OnRequest(request, response){
-    response.send("API by nodejs!");
-    response.end();
+function OnRequest(req, res){
+    const { method = '301' : num = '1' : tag = 'acg' } = req.query;
+    var data = fs.readFileSync('../data/'+tag+'.json');    
+    json_url = JSON.parse(data.toString());
+    res.send(data.toString());
+    res.end();
 }
